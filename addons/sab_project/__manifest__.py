106f815000d5e004b534861212a544532229c3f5
Sascha Preyer <s.preyer@sab-p.de>
2026-08-06 14:18:47 +0200

    Version 0.5.4 - Kalkulationsartikel Action

--- addons/sab_project/__manifest__.py
+++ addons/sab_project/__manifest__.py
@@ -14,6 +14,7 @@
         "data/sab_classification_data.xml",
         "data/sab_calculation_sequence.xml",
         "views/sab_menu_views.xml",
+        "views/sab_calculation_item_views.xml",
         "views/res_config_settings_views.xml",
         "views/project_project_views.xml",
         "views/sab_project_overview_views.xml",
