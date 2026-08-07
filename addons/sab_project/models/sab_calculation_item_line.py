d5a3a1d6c5d762852de72ce7768a12d1818612eb
Sascha Preyer <s.preyer@sab-p.de>
2026-08-07 09:46:17 +0200

    Version 0.7.4 - Produktpositionen Bedienung

--- addons/sab_project/models/sab_calculation_item_line.py
+++ addons/sab_project/models/sab_calculation_item_line.py
@@ -1,14 +1,10 @@
-from odoo import fields, models
+from odoo import api, fields, models
 
 
 class SabCalculationItemLine(models.Model):
     _name = "sab.calculation.item.line"
     _description = "SAB-P Kalkulationszeile"
     _order = "sequence, id"
-
-    # ---------------------------------------------------------
-    # Zugehöriger Kalkulationsartikel
-    # ---------------------------------------------------------
 
     calculation_item_id = fields.Many2one(
         comodel_name="sab.calculation.item",
@@ -18,18 +14,11 @@ class SabCalculationItemLine(models.Model):
         index=True,
     )
 
-    # ---------------------------------------------------------
-    # Reihenfolge
-    # ---------------------------------------------------------
-
     sequence = fields.Integer(
-        string="Position",
+        string="Pos.",
         default=10,
+        index=True,
     )
-
-    # ---------------------------------------------------------
-    # Produkt
-    # ---------------------------------------------------------
 
     product_id = fields.Many2one(
         comodel_name="sab.product",
@@ -37,6 +26,13 @@ class SabCalculationItemLine(models.Model):
         required=True,
         ondelete="restrict",
         index=True,
+    )
+
+    quantity = fields.Float(
+        string="Menge",
+        required=True,
+        default=1.0,
+        digits=(16, 3),
     )
 
     alternative_product_ids = fields.Many2many(
@@ -47,20 +43,6 @@ class SabCalculationItemLine(models.Model):
         string="Alternativprodukte",
     )
 
-    # ---------------------------------------------------------
-    # Menge
-    # ---------------------------------------------------------
-
-    quantity = fields.Float(
-        string="Menge",
-        required=True,
-        default=1.0,
-    )
-
-    # ---------------------------------------------------------
-    # Optionen
-    # ---------------------------------------------------------
-
     optional = fields.Boolean(
         string="Optional",
         default=False,
@@ -69,3 +51,29 @@ class SabCalculationItemLine(models.Model):
     note = fields.Char(
         string="Bemerkung",
     )
+
+    @api.model_create_multi
+    def create(self, vals_list):
+        for vals in vals_list:
+            calculation_item_id = vals.get("calculation_item_id")
+
+            if calculation_item_id and not vals.get("sequence"):
+                last_line = self.search(
+                    [
+                        (
+                            "calculation_item_id",
+                            "=",
+                            calculation_item_id,
+                        )
+                    ],
+                    order="sequence desc, id desc",
+                    limit=1,
+                )
+
+                vals["sequence"] = (
+                    last_line.sequence + 10
+                    if last_line
+                    else 10
+                )
+
+        return super().create(vals_list)
