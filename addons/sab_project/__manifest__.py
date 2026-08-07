bf2b3a6470583d21ac99c1f1b13c767a82865e19
Sascha Preyer <s.preyer@sab-p.de>
2026-08-07 11:09:10 +0200

    Version 0.7.8.1 - Deutsch als Standardsprache

--- addons/sab_project/__manifest__.py
+++ addons/sab_project/__manifest__.py
@@ -16,6 +16,7 @@
 
         "data/sab_classification_data.xml",
         "data/sab_calculation_sequence.xml",
+        "data/sab_default_language.xml",
 
         "views/sab_menu_views.xml",
         "views/sab_calculation_item_views.xml",
