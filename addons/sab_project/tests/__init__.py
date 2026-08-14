14c6e291d2313f3e63abff21f5a7905d438e1580
spreyer-prog <s.preyer@sab-p.de>
2026-08-14 17:06:18 +0200

    Register production workflow tests

--- addons/sab_project/tests/__init__.py
+++ addons/sab_project/tests/__init__.py
@@ -3,3 +3,4 @@ from . import test_calculation_core
 from . import test_datanorm_import
 from . import test_offer_calculation
 from . import test_calculation_audit_and_bom
+from . import test_production_workflow
