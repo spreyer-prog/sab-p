ddfd1969cfb6e90a944b8fb6b0fcf191f38985b1
spreyer-prog <s.preyer@sab-p.de>
2026-08-14 16:55:59 +0200

    Register production workflow and bump module version

--- addons/sab_project/__manifest__.py
+++ addons/sab_project/__manifest__.py
@@ -1,12 +1,13 @@
 {
     "name": "SAB-P Projekt- und Angebotsnummern",
-    "summary": "Projektübersicht, Kalkulationsdatenbank, DATANORM, Angebotskalkulation und Stücklisten für SAB-P",
-    "version": "19.0.5.2.0",
+    "summary": "Projektübersicht, Kalkulationsdatenbank, DATANORM, Angebotskalkulation, Stücklisten und Fertigung für SAB-P",
+    "version": "19.0.5.3.0",
     "category": "Services/Project",
     "author": "SAB-P GmbH",
     "license": "LGPL-3",
 
     "depends": [
+        "mail",
         "project",
         "sale_management",
     ],
@@ -28,6 +29,7 @@
         "views/sab_supplier_product_views.xml",
         "views/sab_calculation_change_log_views.xml",
         "views/sab_project_bom_views.xml",
+        "views/sab_production_order_views.xml",
         "wizard/sab_calculation_import_views.xml",
         "wizard/sab_datanorm_import_views.xml",
     ],
