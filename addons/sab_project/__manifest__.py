7c4174948b8881d386be10c0269f1b28f2e2121d
spreyer-prog <s.preyer@sab-p.de>
2026-08-15 09:26:23 +0200

    Expose project post-calculation reporting

--- addons/sab_project/__manifest__.py
+++ addons/sab_project/__manifest__.py
@@ -1,7 +1,7 @@
 {
     "name": "SAB-P Projekt- und Angebotsnummern",
-    "summary": "Projektübersicht, Kalkulationsdatenbank, DATANORM, Angebotskalkulation, Stücklisten, Fertigung, Einkauf, Lager, Dokumente und Zeiterfassung für SAB-P",
-    "version": "19.0.5.7.0",
+    "summary": "Projektübersicht, Kalkulationsdatenbank, DATANORM, Angebotskalkulation, Stücklisten, Fertigung, Einkauf, Lager, Dokumente, Zeiterfassung und Nachkalkulation für SAB-P",
+    "version": "19.0.5.8.0",
     "category": "Services/Project",
     "author": "SAB-P GmbH",
     "license": "LGPL-3",
@@ -35,6 +35,7 @@
         "views/sab_stock_movement_views.xml",
         "views/sab_project_document_views.xml",
         "views/sab_time_entry_views.xml",
+        "views/sab_project_controlling_views.xml",
         "wizard/sab_calculation_import_views.xml",
         "wizard/sab_datanorm_import_views.xml",
     ],
