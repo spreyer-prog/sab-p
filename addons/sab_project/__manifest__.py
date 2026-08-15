ce8cea3a15819b6065c4871771aedcdd54817990
spreyer-prog <s.preyer@sab-p.de>
2026-08-15 09:24:28 +0200

    Expose service time tracking

--- addons/sab_project/__manifest__.py
+++ addons/sab_project/__manifest__.py
@@ -1,7 +1,7 @@
 {
     "name": "SAB-P Projekt- und Angebotsnummern",
-    "summary": "Projektübersicht, Kalkulationsdatenbank, DATANORM, Angebotskalkulation, Stücklisten, Fertigung, Einkauf, Lager und Dokumente für SAB-P",
-    "version": "19.0.5.6.0",
+    "summary": "Projektübersicht, Kalkulationsdatenbank, DATANORM, Angebotskalkulation, Stücklisten, Fertigung, Einkauf, Lager, Dokumente und Zeiterfassung für SAB-P",
+    "version": "19.0.5.7.0",
     "category": "Services/Project",
     "author": "SAB-P GmbH",
     "license": "LGPL-3",
@@ -34,6 +34,7 @@
         "views/sab_purchase_requirement_views.xml",
         "views/sab_stock_movement_views.xml",
         "views/sab_project_document_views.xml",
+        "views/sab_time_entry_views.xml",
         "wizard/sab_calculation_import_views.xml",
         "wizard/sab_datanorm_import_views.xml",
     ],
