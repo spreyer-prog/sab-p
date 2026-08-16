2e8c3be129a20196f846396682e677312ac2c768
spreyer-prog <s.preyer@sab-p.de>
2026-08-16 21:45:04 +0200

    Load DATANORM enrichment

--- addons/sab_project/wizard/__init__.py
+++ addons/sab_project/wizard/__init__.py
@@ -1,5 +1,6 @@
 from . import sab_calculation_import
 from . import sab_datanorm_import
+from . import sab_datanorm_enrichment
 from . import sab_customer_import
 from . import sab_supplier_import
 from . import sab_supplier_price_import
