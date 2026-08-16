716142e0eda7505e95ad350c5fd00578d48d414e
spreyer-prog <s.preyer@sab-p.de>
2026-08-16 08:24:34 +0200

    Register project customer portal link

--- addons/sab_project/models/__init__.py
+++ addons/sab_project/models/__init__.py
@@ -2,6 +2,7 @@ from . import sab_classification
 from . import project_counter
 from . import res_config_settings
 from . import project_project
+from . import project_customer_portal
 from . import sale_order
 from . import sab_calculation_item
 from . import sab_manufacturer
