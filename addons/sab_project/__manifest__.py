7a6cc0a0d18094cde0be52aa8bc9a0a15c50633a
spreyer-prog <s.preyer@sab-p.de>
2026-08-16 21:43:53 +0200

    Load supplier import actions before supplier views

--- addons/sab_project/__manifest__.py
+++ addons/sab_project/__manifest__.py
@@ -1,7 +1,7 @@
 {
     "name": "SAB-P Projekt- und Angebotsnummern",
     "summary": "SAB-P Suite für Projekt, Kalkulation, Fertigung, Einkauf, Lager, Dokumente, Zeiterfassung, Nachkalkulation, Mitarbeiter-Mobile und Kundenportal",
-    "version": "19.0.5.38.0",
+    "version": "19.0.5.39.0",
     "category": "Services/Project",
     "author": "SAB-P GmbH",
     "license": "LGPL-3",
@@ -12,13 +12,14 @@
         "views/sab_menu_views.xml", "views/sab_employee_admin_views.xml", "views/sab_employee_confidential_views.xml",
         "views/sab_calculation_item_views.xml", "views/res_config_settings_views.xml", "views/project_project_views.xml",
         "views/project_customer_portal_views.xml", "views/sab_project_overview_views.xml", "views/sab_classification_views.xml",
-        "views/sale_order_views.xml", "views/sab_manufacturer_views.xml", "views/sab_product_views.xml", "views/sab_supplier_views.xml",
+        "views/sale_order_views.xml", "views/sab_manufacturer_views.xml", "views/sab_product_views.xml",
+        "wizard/sab_supplier_import_views.xml", "wizard/sab_supplier_price_import_views.xml", "views/sab_supplier_views.xml",
         "views/sab_supplier_product_views.xml", "views/sab_calculation_change_log_views.xml", "views/sab_project_bom_views.xml",
         "views/sab_production_order_views.xml", "views/sab_purchase_requirement_views.xml", "views/sab_stock_movement_views.xml",
         "views/sab_project_document_views.xml", "views/sab_time_entry_views.xml", "views/sab_project_controlling_views.xml",
         "views/sab_employee_mobile_views.xml", "views/sab_employee_feedback_views.xml", "views/sab_customer_status_views.xml",
         "views/sab_customer_portal_templates.xml", "wizard/sab_calculation_import_views.xml", "wizard/sab_datanorm_import_views.xml",
-        "wizard/sab_customer_import_views.xml", "wizard/sab_supplier_import_views.xml"
+        "wizard/sab_customer_import_views.xml"
     ],
     "installable": True,
     "application": False,
