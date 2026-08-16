dfd44dcbc21449ea5db821678dfa65ed38e9b92e
spreyer-prog <s.preyer@sab-p.de>
2026-08-16 13:11:45 +0200

    Add employee administration to SAB-P Suite

--- addons/sab_project/__manifest__.py
+++ addons/sab_project/__manifest__.py
@@ -1,7 +1,7 @@
 {
     "name": "SAB-P Projekt- und Angebotsnummern",
     "summary": "SAB-P Suite für Projekt, Kalkulation, Fertigung, Einkauf, Lager, Dokumente, Zeiterfassung, Nachkalkulation, Mitarbeiter-Mobile und Kundenportal",
-    "version": "19.0.5.22.0",
+    "version": "19.0.5.23.0",
     "category": "Services/Project",
     "author": "SAB-P GmbH",
     "license": "LGPL-3",
@@ -20,7 +20,9 @@
         "data/sab_classification_data.xml",
         "data/sab_calculation_sequence.xml",
         "data/sab_stock_sequence.xml",
+        "data/sab_work_area_data.xml",
         "views/sab_menu_views.xml",
+        "views/sab_employee_admin_views.xml",
         "views/sab_calculation_item_views.xml",
         "views/res_config_settings_views.xml",
         "views/project_project_views.xml",
