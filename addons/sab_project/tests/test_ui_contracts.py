3899e9673bc5e167eee248d75342b2fa18dbbd56
spreyer-prog <s.preyer@sab-p.de>
2026-08-16 13:56:44 +0200

    Test employee access and invitation UI

--- addons/sab_project/tests/test_ui_contracts.py
+++ addons/sab_project/tests/test_ui_contracts.py
@@ -8,7 +8,6 @@ class TestSabUiContracts(TransactionCase):
     def test_project_overview_action_uses_sab_list_view(self):
         action = self.env.ref("sab_project.sab_project_overview_action")
         view = self.env.ref("sab_project.sab_project_overview_list")
-
         self.assertEqual(action.res_model, "project.project")
         self.assertEqual(action.view_mode, "list,form")
         self.assertEqual(action.view_id, view)
@@ -18,7 +17,6 @@ class TestSabUiContracts(TransactionCase):
         view = self.env.ref("sab_project.sab_project_overview_list")
         arch = etree.fromstring(view.arch_db.encode("utf-8"))
         fields = arch.xpath("/list/field")
-
         self.assertTrue(fields)
         self.assertEqual(fields[0].get("name"), "sab_project_reference")
         self.assertIsNone(fields[0].get("optional"))
@@ -27,8 +25,20 @@ class TestSabUiContracts(TransactionCase):
         inherited = self.env.ref("sab_project.sab_standard_project_list_inherit")
         standard = self.env.ref("project.view_project")
         self.assertEqual(inherited.inherit_id, standard)
-
         arch = etree.fromstring(inherited.arch_db.encode("utf-8"))
         inserted = arch.xpath("//field[@name='name']/field[@name='sab_project_reference']")
         self.assertEqual(len(inserted), 1)
         self.assertEqual(inserted[0].get("string"), "Projekt-Nr.")
+
+    def test_employee_admin_exposes_invitation_and_access_status(self):
+        view = self.env.ref("sab_project.view_sab_employee_profile_form")
+        arch = etree.fromstring(view.arch_db.encode("utf-8"))
+        self.assertEqual(len(arch.xpath("//button[@name='action_send_invitation']")), 1)
+        self.assertEqual(len(arch.xpath("//field[@name='user_state']")), 1)
+        self.assertEqual(len(arch.xpath("//field[@name='last_login']")), 1)
+
+    def test_production_ui_uses_work_area_and_employee_profile(self):
+        view = self.env.ref("sab_project.view_sab_production_order_form")
+        arch = etree.fromstring(view.arch_db.encode("utf-8"))
+        self.assertTrue(arch.xpath("//field[@name='step_ids']//field[@name='work_area_id']"))
+        self.assertTrue(arch.xpath("//field[@name='step_ids']//field[@name='responsible_employee_id']"))
