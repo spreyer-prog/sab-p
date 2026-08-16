a344e23b101c8f3822494f9fc678081aa16d2f9e
spreyer-prog <s.preyer@sab-p.de>
2026-08-16 15:03:41 +0200

    Advance employee impersonation hardening version

--- addons/sab_project/__manifest__.py
+++ addons/sab_project/__manifest__.py
@@ -1,7 +1,7 @@
 {
     "name": "SAB-P Projekt- und Angebotsnummern",
     "summary": "SAB-P Suite für Projekt, Kalkulation, Fertigung, Einkauf, Lager, Dokumente, Zeiterfassung, Nachkalkulation, Mitarbeiter-Mobile und Kundenportal",
-    "version": "19.0.5.33.0",
+    "version": "19.0.5.34.0",
     "category": "Services/Project",
     "author": "SAB-P GmbH",
     "license": "LGPL-3",
@@ -21,5 +21,5 @@
     ],
     "installable": True,
     "application": False,
-    "auto_install": False,
+    "auto_install": False
 }
