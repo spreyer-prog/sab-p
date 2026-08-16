4a0b7f1daa4a56660dcf26aca75036aa6083757e
spreyer-prog <s.preyer@sab-p.de>
2026-08-16 18:20:38 +0200

    Link SAB-P suppliers to contact master data

--- addons/sab_project/models/sab_supplier.py
+++ addons/sab_project/models/sab_supplier.py
@@ -7,27 +7,17 @@ class SabSupplier(models.Model):
     _order = "name"
     _rec_name = "name"
 
-    active = fields.Boolean(
-        string="Aktiv",
-        default=True,
-    )
-
-    name = fields.Char(
-        string="Lieferant",
-        required=True,
+    active = fields.Boolean(string="Aktiv", default=True)
+    name = fields.Char(string="Lieferant", required=True, index=True)
+    supplier_number = fields.Char(string="Lieferantennummer", index=True)
+    partner_id = fields.Many2one(
+        comodel_name="res.partner",
+        string="Kontakt-/Adressstamm",
+        ondelete="restrict",
         index=True,
+        help="Verknüpfter Odoo-Kontakt für Adresse, Ansprechpartner und Bankverbindungen.",
     )
-
-    supplier_number = fields.Char(
-        string="Lieferantennummer",
-        index=True,
-    )
-
-    datanorm_identifier = fields.Char(
-        string="DATANORM-Kennung",
-        index=True,
-    )
-
+    datanorm_identifier = fields.Char(string="DATANORM-Kennung", index=True)
     datanorm_price_as_purchase_price = fields.Boolean(
         string="DATANORM-Preis als EK übernehmen",
         default=False,
@@ -38,11 +28,5 @@ class SabSupplier(models.Model):
             "unabhängig davon immer gespeichert."
         ),
     )
-
-    website = fields.Char(
-        string="Website",
-    )
-
-    note = fields.Text(
-        string="Interne Hinweise",
-    )
+    website = fields.Char(string="Website")
+    note = fields.Text(string="Interne Hinweise")
