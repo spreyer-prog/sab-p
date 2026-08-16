2967df7bffefd10826c39f4fe2d0e0e4195f965b
spreyer-prog <s.preyer@sab-p.de>
2026-08-16 09:15:41 +0200

    Register security permission tests

--- addons/sab_project/tests/__init__.py
+++ addons/sab_project/tests/__init__.py
@@ -10,3 +10,4 @@ from . import test_project_document
 from . import test_time_entry
 from . import test_employee_feedback
 from . import test_customer_status
+from . import test_security_permissions
