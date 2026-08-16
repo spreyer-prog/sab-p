fc0cd16e8fa7fe6504e78ece0f16509018e1fe3e
spreyer-prog <s.preyer@sab-p.de>
2026-08-16 08:25:16 +0200

    Test project customer status shortcut

--- addons/sab_project/tests/test_customer_status.py
+++ addons/sab_project/tests/test_customer_status.py
@@ -13,15 +13,17 @@ class TestSabCustomerStatus(TransactionCase):
         })
 
     def test_customer_status_requires_explicit_release_after_change(self):
-        status = self.env["sab.customer.project.status"].create({
-            "project_id": self.project.id,
-            "milestone": "order_received",
-            "note_customer": "Auftrag ist eingegangen.",
-            "note_internal": "Interner Text darf nicht zum Kunden.",
-        })
+        action = self.project.action_open_sab_customer_status()
+        status = self.env["sab.customer.project.status"].browse(action["res_id"])
+        self.assertEqual(status.project_id, self.project)
+        self.assertEqual(self.project.sab_customer_status_count, 1)
         self.assertFalse(status.released)
         self.assertEqual(status.progress_percent, 0)
 
+        status.write({
+            "note_customer": "Auftrag ist eingegangen.",
+            "note_internal": "Interner Text darf nicht zum Kunden.",
+        })
         status.action_release()
         self.assertTrue(status.released)
         self.assertTrue(status.released_at)
@@ -32,3 +34,7 @@ class TestSabCustomerStatus(TransactionCase):
 
         status.action_release()
         self.assertTrue(status.released)
+
+        second_action = self.project.action_open_sab_customer_status()
+        self.assertEqual(second_action["res_id"], status.id)
+        self.assertEqual(self.project.sab_customer_status_count, 1)
