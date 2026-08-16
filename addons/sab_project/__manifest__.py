86132b6ea0b86bc4b9a0d256cab6ba999d2e097c
spreyer-prog <s.preyer@sab-p.de>
2026-08-16 13:17:12 +0200

    Add confidential personnel records to SAB-P employee management

--- addons/sab_project/__manifest__.py
+++ addons/sab_project/__manifest__.py
@@ -1,7 +1,7 @@
 {
     "name": "SAB-P Projekt- und Angebotsnummern",
     "summary": "SAB-P Suite für Projekt, Kalkulation, Fertigung, Einkauf, Lager, Dokumente, Zeiterfassung, Nachkalkulation, Mitarbeiter-Mobile und Kundenportal",
-    "version": "19.0.5.23.0",
+    "version": "19.0.5.24.0",
     "category": "Services/Project",
     "author": "SAB-P GmbH",
     "license": "LGPL-3",
@@ -23,6 +23,7 @@
         "data/sab_work_area_data.xml",
         "views/sab_menu_views.xml",
         "views/sab_employee_admin_views.xml",
+        "views/sab_employee_confidential_views.xml",
         "views/sab_calculation_item_views.xml",
         "views/res_config_settings_views.xml",
         "views/project_project_views.xml",
