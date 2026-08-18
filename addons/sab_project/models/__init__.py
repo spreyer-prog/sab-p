036788f8aebb0a6d385659078fbd64cbc2d2a78f
spreyer-prog <s.preyer@sab-p.de>
2026-08-18 15:34:39 +0200

    Load calculation component reuse library models

--- addons/sab_project/models/__init__.py
+++ addons/sab_project/models/__init__.py
@@ -24,6 +24,7 @@ from . import sab_offer_calculation_lv
 from . import sab_offer_switchboard
 from . import sab_offer_schematic_lv
 from . import sab_offer_calculation_component
+from . import sab_offer_reuse_library
 from . import sab_calculation_change_log
 from . import sab_project_bom
 from . import sab_employee
