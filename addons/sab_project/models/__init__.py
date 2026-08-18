56793ccc4fc11fe04b692021b4b25e656d136655
spreyer-prog <s.preyer@sab-p.de>
2026-08-18 12:41:34 +0200

    Load LV enforcement extension

--- addons/sab_project/models/__init__.py
+++ addons/sab_project/models/__init__.py
@@ -18,6 +18,7 @@ from . import sab_datanorm_surcharge
 from . import sab_offer_calculation_group
 from . import sab_project_lv_mapping
 from . import sab_offer_calculation_line
+from . import sab_offer_calculation_lv
 from . import sab_offer_calculation_component
 from . import sab_calculation_change_log
 from . import sab_project_bom
