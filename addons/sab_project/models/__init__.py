1a5bdb34d4976bb50bb0ea6414fc9eb9ed81ae33
spreyer-prog <s.preyer@sab-p.de>
2026-08-18 07:41:06 +0200

    Load SAB offer calculation groups

--- addons/sab_project/models/__init__.py
+++ addons/sab_project/models/__init__.py
@@ -12,6 +12,7 @@ from . import sab_calculation_item_line
 from . import sab_supplier
 from . import sab_supplier_product
 from . import sab_datanorm_surcharge
+from . import sab_offer_calculation_group
 from . import sab_offer_calculation_line
 from . import sab_offer_calculation_component
 from . import sab_calculation_change_log
