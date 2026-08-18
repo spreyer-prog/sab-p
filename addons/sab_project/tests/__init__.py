d5661730c57002822a0552a05c754a5eaeb25e28
spreyer-prog <s.preyer@sab-p.de>
2026-08-18 13:32:48 +0200

    Register project LV mapping regression tests

--- addons/sab_project/tests/__init__.py
+++ addons/sab_project/tests/__init__.py
@@ -4,6 +4,7 @@ from . import test_datanorm_import
 from . import test_customer_import
 from . import test_supplier_import
 from . import test_offer_calculation
+from . import test_project_lv_mapping
 from . import test_calculation_audit_and_bom
 from . import test_production_workflow
 from . import test_employee_production_start
