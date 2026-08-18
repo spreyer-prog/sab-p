25b2c29f394f79200f16b828585e634258a8a7c5
spreyer-prog <s.preyer@sab-p.de>
2026-08-18 09:39:08 +0200

    Load Odoo product SAB-P extension

--- addons/sab_project/models/__init__.py
+++ addons/sab_project/models/__init__.py
@@ -7,6 +7,7 @@ from . import sale_order
 from . import sab_sale_order_template
 from . import sab_calculation_item
 from . import sab_manufacturer
+from . import product_product_extension
 from . import sab_product
 from . import sab_product_component
 from . import sab_calculation_item_line
