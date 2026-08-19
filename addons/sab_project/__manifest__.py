e25575c8ec072719f99c5a61332a054ed4a38a00
spreyer-prog <s.preyer@sab-p.de>
2026-08-19 19:24:45 +0200

    Fix ACL data filename and bump SAB-P Suite

--- addons/sab_project/__manifest__.py
+++ addons/sab_project/__manifest__.py
@@ -1,13 +1,13 @@
 {
     "name": "SAB-P Suite",
     "summary": "SAB-P Suite für Projekt, Kalkulation, Fertigung, Einkauf, Lager, Dokumente, Zeiterfassung, Nachkalkulation, Mitarbeiter-Mobile und Kundenportal",
-    "version": "19.0.5.53.2",
+    "version": "19.0.5.53.3",
     "category": "Services/Project",
     "author": "SAB-P GmbH",
     "license": "LGPL-3",
     "depends": ["auth_signup", "mail", "portal", "project", "sale_management"],
     "data": [
-        "security/sab_security.xml", "security/ir.model.access.csv", "security/ir.model.access.procurement_package.csv", "security/sab_record_rules.xml",
+        "security/sab_security.xml", "security/ir.model.access.csv", "security/sab_record_rules.xml",
         "data/sab_classification_data.xml", "data/sab_calculation_sequence.xml", "data/sab_stock_sequence.xml", "data/sab_procurement_request_sequence.xml", "data/sab_work_area_data.xml",
         "data/sab_purchase_mail_template.xml", "data/sab_delivery_overdue_data.xml",
         "views/sab_menu_views.xml", "views/sab_employee_admin_views.xml", "views/sab_employee_confidential_views.xml",
