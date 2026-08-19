b53521ff11048543ec98797512bbdb528bdc8369
spreyer-prog <s.preyer@sab-p.de>
2026-08-19 23:15:09 +0200

    Release SAB-P Suite 19.0.5.53.5 test setup fix

--- addons/sab_project/__manifest__.py
+++ addons/sab_project/__manifest__.py
@@ -1,7 +1,7 @@
 {
     "name": "SAB-P Suite",
     "summary": "SAB-P Suite für Projekt, Kalkulation, Fertigung, Einkauf, Lager, Dokumente, Zeiterfassung, Nachkalkulation, Mitarbeiter-Mobile und Kundenportal",
-    "version": "19.0.5.53.4",
+    "version": "19.0.5.53.5",
     "category": "Services/Project",
     "author": "SAB-P GmbH",
     "license": "LGPL-3",
