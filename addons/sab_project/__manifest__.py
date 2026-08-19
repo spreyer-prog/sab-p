cd133d6bfa87c9b90ddb59edabddeb18a301edbd
spreyer-prog <s.preyer@sab-p.de>
2026-08-19 09:25:46 +0200

    Release hardened procurement workflow 19.0.5.52.1

--- addons/sab_project/__manifest__.py
+++ addons/sab_project/__manifest__.py
@@ -1,7 +1,7 @@
 {
     "name": "SAB-P Suite",
     "summary": "SAB-P Suite für Projekt, Kalkulation, Fertigung, Einkauf, Lager, Dokumente, Zeiterfassung, Nachkalkulation, Mitarbeiter-Mobile und Kundenportal",
-    "version": "19.0.5.52.0",
+    "version": "19.0.5.52.1",
     "category": "Services/Project",
     "author": "SAB-P GmbH",
     "license": "LGPL-3",
@@ -18,7 +18,7 @@
         "wizard/sab_supplier_import_views.xml", "wizard/sab_supplier_price_import_views.xml", "views/sab_supplier_views.xml",
         "views/sab_supplier_product_views.xml", "views/sab_calculation_change_log_views.xml", "views/sab_project_bom_views.xml", "views/sab_switchboard_bom_views.xml",
         "views/sab_production_order_views.xml", "views/sab_purchase_requirement_views.xml", "views/sab_stock_movement_views.xml", "views/sab_procurement_views.xml",
-        "views/sab_procurement_detail_views.xml",
+        "views/sab_procurement_detail_views.xml", "views/sab_project_material_overview_views.xml",
         "report/sab_purchase_order_report.xml", "report/sab_purchase_order_delivery_report.xml",
         "views/sab_project_document_views.xml", "views/sab_time_entry_views.xml", "views/sab_project_controlling_views.xml",
         "views/sab_employee_mobile_views.xml", "views/sab_employee_feedback_views.xml", "views/sab_customer_status_views.xml",
