3749961be5d456f754b294bc05e1a97d0e13ff13
spreyer-prog <s.preyer@sab-p.de>
2026-08-19 23:37:23 +0200

    Load procurement material action domain fix

--- addons/sab_project/models/__init__.py
+++ addons/sab_project/models/__init__.py
@@ -59,3 +59,4 @@ from . import sab_minimum_stock_fix
 from . import sab_delivery_overdue
 from . import sab_delivery_overdue_fix
 from . import sab_procurement_regression_fixes
+from . import sab_procurement_action_domain_fix
