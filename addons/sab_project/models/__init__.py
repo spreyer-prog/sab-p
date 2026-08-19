0b389b8f7226ac6f6a63a055b080b5766a4f0b05
spreyer-prog <s.preyer@sab-p.de>
2026-08-19 18:53:55 +0200

    Load corrected minimum-stock projection

--- addons/sab_project/models/__init__.py
+++ addons/sab_project/models/__init__.py
@@ -55,5 +55,6 @@ from . import sab_procurement_package
 from . import sab_procurement_package_selection_fix
 from . import sab_procurement_package_fix
 from . import sab_minimum_stock
+from . import sab_minimum_stock_fix
 from . import sab_delivery_overdue
 from . import sab_delivery_overdue_fix
