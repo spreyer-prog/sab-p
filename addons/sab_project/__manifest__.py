f95ee91b557fcd642a9487a71148f880d6e5a546
spreyer-prog <s.preyer@sab-p.de>
2026-08-19 09:57:36 +0200

    Release procurement hardening 19.0.5.52.2

--- addons/sab_project/__manifest__.py
+++ addons/sab_project/__manifest__.py
@@ -1,7 +1,7 @@
 {
     "name": "SAB-P Suite",
     "summary": "SAB-P Suite für Projekt, Kalkulation, Fertigung, Einkauf, Lager, Dokumente, Zeiterfassung, Nachkalkulation, Mitarbeiter-Mobile und Kundenportal",
-    "version": "19.0.5.52.1",
+    "version": "19.0.5.52.2",
     "category": "Services/Project",
     "author": "SAB-P GmbH",
     "license": "LGPL-3",
