bc62156b5355ce6b9ffafe1b1e002beb0c59525e
spreyer-prog <s.preyer@sab-p.de>
2026-08-19 10:02:03 +0200

    Load procurement transition security rules

--- addons/sab_project/models/__init__.py
+++ addons/sab_project/models/__init__.py
@@ -39,6 +39,7 @@ from . import sab_procurement
 from . import project_procurement
 from . import sab_procurement_corrections
 from . import sab_procurement_roles
+from . import sab_procurement_transition_security
 from . import project_procurement_detail
 from . import sab_project_document
 from . import sab_time_entry
