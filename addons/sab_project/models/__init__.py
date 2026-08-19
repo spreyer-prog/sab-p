87664064d48e5e8909bcdd334ed7d3c550473663
spreyer-prog <s.preyer@sab-p.de>
2026-08-19 17:44:41 +0200

    Load commercial offer line release security

--- addons/sab_project/models/__init__.py
+++ addons/sab_project/models/__init__.py
@@ -27,6 +27,7 @@ from . import sab_offer_component_context
 from . import sab_offer_calculation_component
 from . import sab_offer_reuse_library
 from . import sab_offer_release_workflow
+from . import sab_offer_release_order_line_security
 from . import sab_calculation_change_log
 from . import sab_project_bom
 from . import sab_employee
