1f4dfd72d8a87184084d962f60da9d64ef284af6
spreyer-prog <s.preyer@sab-p.de>
2026-08-20 12:21:21 +0200

    Load standard purchase bridge after legacy procurement extensions

--- addons/sab_project/models/__init__.py
+++ addons/sab_project/models/__init__.py
@@ -60,3 +60,5 @@ from . import sab_delivery_overdue
 from . import sab_delivery_overdue_fix
 from . import sab_procurement_regression_fixes
 from . import sab_procurement_action_domain_fix
+from . import sab_odoo_purchase_bridge
+from . import sab_odoo_purchase_bridge_runtime
