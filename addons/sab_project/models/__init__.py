a413377305f863a64a8d9a3b393a7fe6914016ef
spreyer-prog <s.preyer@sab-p.de>
2026-08-20 17:51:19 +0200

    Load SAB-P commissioning picking type setup

--- addons/sab_project/models/__init__.py
+++ addons/sab_project/models/__init__.py
@@ -86,3 +86,4 @@ from . import sab_supplier_confirmation
 from . import sab_odoo_stock_destination_fix
 from . import sab_legacy_purchase_status_compat
 from . import sab_offer_section_parent_fix
+from . import sab_odoo_commissioning_picking_type
