0ca964d1c75924988fab4ed39b5b959d13851d40
spreyer-prog <s.preyer@sab-p.de>
2026-08-20 12:26:06 +0200

    Load total-BOM procurement package correction last

--- addons/sab_project/models/__init__.py
+++ addons/sab_project/models/__init__.py
@@ -62,3 +62,4 @@ from . import sab_procurement_regression_fixes
 from . import sab_procurement_action_domain_fix
 from . import sab_odoo_purchase_bridge
 from . import sab_odoo_purchase_bridge_runtime
+from . import sab_odoo_purchase_package_fix
