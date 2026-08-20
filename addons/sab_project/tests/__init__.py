11578b0360e3123a2924290a3367764b7eb59ba3
spreyer-prog <s.preyer@sab-p.de>
2026-08-20 13:36:51 +0200

    Load mandatory standard purchase approval test

--- addons/sab_project/tests/__init__.py
+++ addons/sab_project/tests/__init__.py
@@ -32,3 +32,4 @@ from . import test_minimum_stock_and_delivery_overdue
 from . import test_standard_purchase_bridge
 from . import test_standard_commissioning_bridge
 from . import test_standard_procurement_role_separation
+from . import test_standard_purchase_approval
