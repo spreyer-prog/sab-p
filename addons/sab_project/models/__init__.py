01676f51a4c5f46c88c94a7c261ad85104409c27
spreyer-prog <s.preyer@sab-p.de>
2026-08-20 13:57:42 +0200

    Load SAB-P purchase action security last

--- addons/sab_project/models/__init__.py
+++ addons/sab_project/models/__init__.py
@@ -77,3 +77,4 @@ from . import sab_odoo_commissioning_security_fix
 from . import sab_odoo_purchase_approval
 from . import sab_odoo_receipt_security_fix
 from . import sab_odoo_purchase_approver_edit_security
+from . import sab_odoo_purchase_action_security
