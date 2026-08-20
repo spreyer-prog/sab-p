27c0628d68cb6b8bb8a375d2153d273f2313b74f
spreyer-prog <s.preyer@sab-p.de>
2026-08-20 13:15:22 +0200

    Load Odoo unit-of-measure bridge last

--- addons/sab_project/models/__init__.py
+++ addons/sab_project/models/__init__.py
@@ -70,3 +70,4 @@ from . import sab_odoo_stock_bridge_fix
 from . import sab_odoo_stock_field_fix
 from . import sab_odoo_commissioning_context_fix
 from . import sab_odoo_storable_product
+from . import sab_odoo_uom_bridge
