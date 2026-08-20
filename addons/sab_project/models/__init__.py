59d8d6d26c52337fb394f86ec438c04694225b5a
spreyer-prog <s.preyer@sab-p.de>
2026-08-20 14:26:24 +0200

    Load production document model

--- addons/sab_project/models/__init__.py
+++ addons/sab_project/models/__init__.py
@@ -33,6 +33,7 @@ from . import sab_project_bom
 from . import sab_employee
 from . import sab_employee_confidential
 from . import sab_production_order
+from . import sab_production_document
 from . import sab_production_employee_runtime
 from . import sab_purchase_requirement
 from . import sab_switchboard_bom
