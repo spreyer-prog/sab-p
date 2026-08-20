48e3bc316bd4d29cec128a835af101f32e9e5831
spreyer-prog <s.preyer@sab-p.de>
2026-08-20 13:54:27 +0200

    Load pure approver edit-security test

--- addons/sab_project/tests/__init__.py
+++ addons/sab_project/tests/__init__.py
@@ -34,3 +34,4 @@ from . import test_standard_commissioning_bridge
 from . import test_standard_procurement_role_separation
 from . import test_standard_purchase_approval
 from . import test_standard_receipt_security
+from . import test_standard_purchase_approver_edit_security
