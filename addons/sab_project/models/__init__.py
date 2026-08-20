1d3d2faa2eff00a4c679b897a698c75603356fa3
spreyer-prog <s.preyer@sab-p.de>
2026-08-20 18:06:30 +0200

    Load procurement order quantity release fix

--- addons/sab_project/models/__init__.py
+++ addons/sab_project/models/__init__.py
@@ -89,3 +89,4 @@ from . import sab_offer_section_parent_fix
 from . import sab_odoo_commissioning_picking_type
 from . import sab_offer_section_membership_final
 from . import sab_odoo_receipt_reservation_fix
+from . import sab_procurement_order_quantity_release_fix
