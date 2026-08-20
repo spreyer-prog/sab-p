4ab86473fd90a69f1dac05dedc137a38a04db06b
spreyer-prog <s.preyer@sab-p.de>
2026-08-20 17:08:44 +0200

    Load legacy purchase status compatibility last

--- addons/sab_project/models/__init__.py
+++ addons/sab_project/models/__init__.py
@@ -84,3 +84,4 @@ from . import sab_odoo_uom_19_compat
 from . import sab_three_way_match
 from . import sab_supplier_confirmation
 from . import sab_odoo_stock_destination_fix
+from . import sab_legacy_purchase_status_compat
