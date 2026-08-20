8961b483a0d88927704bf796528cabd8b30f172f
spreyer-prog <s.preyer@sab-p.de>
2026-08-20 13:07:08 +0200

    Load commissioning context correction last

--- addons/sab_project/models/__init__.py
+++ addons/sab_project/models/__init__.py
@@ -68,3 +68,4 @@ from . import sab_odoo_purchase_cancellation_fix
 from . import sab_odoo_stock_bridge
 from . import sab_odoo_stock_bridge_fix
 from . import sab_odoo_stock_field_fix
+from . import sab_odoo_commissioning_context_fix
