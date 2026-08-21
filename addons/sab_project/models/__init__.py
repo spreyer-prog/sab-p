e1bff41c5ad69691292b5f770b29d1772f62eff2
spreyer-prog <s.preyer@sab-p.de>
2026-08-21 17:07:54 +0200

    Load SAB-P purchase admin access override

--- addons/sab_project/models/__init__.py
+++ addons/sab_project/models/__init__.py
@@ -96,3 +96,4 @@ from . import sab_procurement_order_quantity_release_fix
 from . import sab_odoo_backorder_assignment_fix
 from . import sab_odoo_supplier_return_assignment_fix
 from . import sab_procurement_standard_order_redirect
+from . import sab_purchase_admin_access
