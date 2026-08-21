e6c923000b8175bffd8833fdef9f3cd0ca7474aa
spreyer-prog <s.preyer@sab-p.de>
2026-08-21 09:55:37 +0200

    Load supplier choice wizard

--- addons/sab_project/wizard/__init__.py
+++ addons/sab_project/wizard/__init__.py
@@ -7,3 +7,4 @@ from . import sab_supplier_price_import
 from . import sab_procurement_package_wizard
 from . import sab_procurement_extra_line_wizard
 from . import sab_production_print_wizard
+from . import sab_supplier_choice_wizard
