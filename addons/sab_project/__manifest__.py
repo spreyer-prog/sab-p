cbd9adce24db3a6be52a0785f981a893aadd6385
spreyer-prog <s.preyer@sab-p.de>
2026-08-22 13:01:26 +0200

    Fertigungsdruck: Version auf 19.0.5.77.0 erhöhen

--- addons/sab_project/__manifest__.py
+++ addons/sab_project/__manifest__.py
@@ -1,7 +1,7 @@
 {
     "name": "SAB-P Suite",
     "summary": "SAB-P Suite für Projekt, Kalkulation, Fertigung, Einkauf, Lager, Dokumente, Zeiterfassung, Nachkalkulation, Mitarbeiter-Mobile und Kundenportal",
-    "version": "19.0.5.76.0",
+    "version": "19.0.5.77.0",
     "category": "Services/Project",
     "author": "SAB-P GmbH",
     "license": "LGPL-3",
