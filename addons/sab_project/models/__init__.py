7a5c2215d9f6a80b6b4e281177712ef07fa8e445
spreyer-prog <s.preyer@sab-p.de>
2026-08-22 14:59:54 +0200

    Load total BOM release workflow

--- addons/sab_project/models/__init__.py
+++ addons/sab_project/models/__init__.py
@@ -104,3 +104,4 @@ from . import sab_bom_regeneration_reference_guard
 from . import sab_default_language_all_users
 from . import sab_purchase_bulk_and_merge
 from . import sab_production_print_profile
+from . import sab_total_bom_release_flow
