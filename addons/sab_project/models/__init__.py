235d64f8ed0fdf18f54f0f1748179916b9dff630
spreyer-prog <s.preyer@sab-p.de>
2026-08-22 14:17:57 +0200

    Load BOM regeneration reference guard

--- addons/sab_project/models/__init__.py
+++ addons/sab_project/models/__init__.py
@@ -100,3 +100,4 @@ from . import sab_odoo_supplier_return_assignment_fix
 from . import sab_procurement_standard_order_redirect
 from . import sab_purchase_admin_access
 from . import sab_standard_purchase_release_compat
+from . import sab_bom_regeneration_reference_guard
